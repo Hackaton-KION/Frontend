91b8bcc143b06a5594f37fd51cb2366d3f0d3744
Bricks <76704453+Bricks666@users.noreply.github.com>
2023-03-24 19:14:46 +0300

    Инит

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import * as path from 'path';
+import { babel } from '@rollup/plugin-babel';
+import react from '@vitejs/plugin-react';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		port: 3000,
+		cors: true,
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	css: {
+		devSourcemap: true,
+	},
+	plugins: [
+		react(),
+		babel({
+			babelrc: true,
+			configFile: true,
+			babelHelpers: 'bundled',
+			browserslistConfigFile: true,
+			extensions: ['.ts', '.tsx'],
+		}),
+		splitVendorChunkPlugin()
+	],
+});
